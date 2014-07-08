9e36ede94b36cadffcf37485dee4e55910c90d98
arvin <webappsniper@gmail.com>
2014-07-08 14:35:21 +0800

    增加topbar的滚动隐藏效果

---
+++ js/jquery.topbar.js
@@ -0,0 +1,24 @@
+(function($){
+	$.fn.topbar = function(){
+		var pre = 0, 
+			cur = 0, 
+			$topbar = this;
+		$(window).scroll(function(){
+			cur = $(this).scrollTop();
+			if (cur >= pre) {
+				//向下滚动
+				// $topbar.fadeOut();
+				$topbar.slideUp('fast');
+			} else if (cur < pre) {
+				//向上滚动
+				//$topbar.fadeIn();
+				$topbar.slideDown('fast');
+			} else {
+				//未能捕获的情况
+				console.log('topbar: 滚动异常');
+			}
+			pre = cur;
+		});
+		return this;
+	};
+})(jQuery);
